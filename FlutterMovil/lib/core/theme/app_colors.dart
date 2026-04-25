e99793a7a45c2bbb9f480f434b3e49c4c9c10e8c
OmarA333 <gonzalez.andres890@gmail.com>
2026-04-24 21:09:01 -0500

    colores

--- FlutterMovil/lib/core/theme/app_colors.dart
+++ FlutterMovil/lib/core/theme/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary     = Color(0xFFDC2626); // Red
-  static const Color secondary   = Color(0xFF1E293B); // Slate 800
+  static const Color primary     = Color.fromARGB(255, 46, 7, 7); // Red
+  static const Color secondary   = Color.fromARGB(255, 134, 13, 13); // Slate 800
   static const Color background  = Color(0xFFF8FAFC); // Slate 50
   static const Color surface     = Color(0xFFFFFFFF); // White
   static const Color text        = Color(0xFF0F172A); // Slate 900
